6beee8d1b4ea718dc57d5ba2c26d109b3884c5b1
Balázs Antal <balazsantal1999@gmail.com>
2021-05-14 11:11:43 +0200

    GameModel created.

--- src/main/java/game/model/GameModel.java
+++ src/main/java/game/model/GameModel.java
@@ -1,4 +1,40 @@
 package game.model;
 
+import lombok.Data;
+
+@Data
+
 public class GameModel {
+    private int[][] grid;
+
+    private String p1name;
+
+    private String p2name;
+
+    private int p1steps;
+
+    private int p2steps;
+
+    private String winnerName;
+
+    private int winnerSteps;
+
+
+
+    public GameModel() {
+        this.grid = new int[][]{
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}
+        };
+    }
+
 }
